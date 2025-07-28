756577c0a9566e7a7895661a58120e5d810c7c8a
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-07-28 14:24:56 +0600

    padding px-4 in title

--- src/components/section-title/SectionTitle.tsx
+++ src/components/section-title/SectionTitle.tsx
@@ -16,7 +16,7 @@ export default function SectionTitle({
   };
 
   return (
-    <div className={`my-8 ${alignment[align]}`}>
+    <div className={`my-8 ${alignment[align]} px-4`}>
       <h2 className="text-3xl font-bold text-gray-800">{title}</h2>
       {subtitle && <p className="text-gray-600 mt-2">{subtitle}</p>}
     </div>
